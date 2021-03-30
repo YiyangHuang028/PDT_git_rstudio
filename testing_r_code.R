ecf819187005a24915269ffbfd237f3a16bb29e5
Jennifer Su <Jennifer@MacBook-Pro.local>
2021-03-30 02:51:33 -0400

    create an expoenetial distribution

--- testing_r_code.R
+++ testing_r_code.R
@@ -6,3 +6,5 @@ num <- rnorm(100, 0, 1)
 
 hist(num, xlab = "number", main = "Histogram of 100 normally distributed values")
 
+num_2 <- rexp(n = 10,rate = 1/3)
+hist(num_2, xlab = "number", main = "Histogram of 10 exponentially distributed values")
