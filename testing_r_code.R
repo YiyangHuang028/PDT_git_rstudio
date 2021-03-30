7b96a261b953630f60e8fbdc5899b16e0df6f8c4
Yiyang Huang <yiyanghuang@Yiyangs-MacBook-Air.local>
2021-03-30 14:51:41 +0800

    add new plot(poisson)

--- testing_r_code.R
+++ testing_r_code.R
@@ -2,7 +2,14 @@ a <- c(1,2,3)
 b <- a/2
 b
 
+#Normal distribution
 num <- rnorm(100, 0, 1)
+hist(num, 
+     xlab = "number", 
+     main = "Histogram of 100 normally distributed values")
 
-hist(num, xlab = "number", main = "Histogram of 100 normally distributed values")
-
+#Poisson distribution
+num_p <- rnorm(100, 2)
+hist(num_p, 
+     xlab = "number", 
+     main = "Histogram of 100 values with poisson distribution")
